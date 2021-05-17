4075181ac6468c3c74dab21af08a292a425760b8
nkusumo <nis.kusumo@gmail.com>
2021-05-17 12:52:19 -0700

    commit tree 544f8c8d0f5686aabfe57a254e9f28f235556b05

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet (pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet (starting, ending) {
+    return Math.abs(starting - ending) * 264;
+}
+
+function calculatesFarePrice (starting, ending) {
+    let distance = distanceTravelledInFeet (starting, ending);
+    if (distance < 400) {
+        return 0;
+    } else if (distance <= 2000) {
+        return (distance - 400) * .02;
+    } else if (distance < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
